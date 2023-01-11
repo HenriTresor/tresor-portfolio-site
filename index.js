0a188f9543cd9ab87a9fce3842bc55b5442d002a
henri tresor <shimwamanahenritresor@gmail.com>
2023-01-11 11:23:54 +0200

    portfolio site

---
+++ index.js
@@ -0,0 +1,78 @@
+let a = document.querySelectorAll(".nav-item");
+let divContent = document.querySelectorAll(".div-content");
+for (let i = 0; i < a.length; i++) {
+  a[i].addEventListener("click", (e) => {
+    e.preventDefault();
+  });
+}
+
+window.addEventListener("load", () => {
+  for (let j = 0; j < divContent.length; j++) {
+    divContent[j].classList.add("hidden");
+    document.querySelector(".welcome-msg").classList.remove("hidden");
+    document.querySelector(".intro-div").classList.add("hidden");
+  }
+});
+
+a[0].addEventListener("click", function () {
+  for (let j = 0; j < divContent.length; j++) {
+    divContent[j].classList.add("hidden");
+    document.querySelector(".welcome-msg").classList.add("hidden");
+    document.querySelector(".intro-div").classList.remove("hidden");
+  }
+});
+
+a[1].addEventListener("click", function () {
+  for (let j = 0; j < divContent.length; j++) {
+    divContent[j].classList.add("hidden");
+    document.querySelector(".welcome-msg").classList.add("hidden");
+    document.querySelector(".skill-div").classList.remove("hidden");
+  }
+});
+
+a[2].addEventListener("click", function () {
+  for (let j = 0; j < divContent.length; j++) {
+    divContent[j].classList.add("hidden");
+    document.querySelector(".welcome-msg").classList.add("hidden");
+    document.querySelector(".project-div").classList.remove("hidden");
+  }
+});
+
+a[3].addEventListener("click", function () {
+  for (let j = 0; j < divContent.length; j++) {
+    divContent[j].classList.add("hidden");
+    document.querySelector(".welcome-msg").classList.add("hidden");
+    document.querySelector(".cont-div").classList.remove("hidden");
+  }
+});
+
+// hamburger
+
+let hamburger = document.querySelector(".hamburger");
+hamburger.addEventListener("click", () => {
+  let navbar = document.querySelector(".navbar");
+  navbar.classList.toggle("active");
+  hamburger.classList.toggle("active");
+});
+
+let links = document.querySelector(".a");
+
+for (let i = 0; i < links.length; i++) {
+  links[i].addEventListener("click", (e) => {
+    e.preventDefault();
+  });
+}
+let randDiv = Math.floor(Math.random() * divContent.length);
+let currentDiv = divContent[randDiv]
+for (let u = 0; u < divContent.length; u++) {
+  if (divContent[u].classList.contains("welcome-msg")) {
+    let contBtn = document.querySelector(".continue-btn");
+    contBtn.addEventListener("click", () => {
+      document.querySelector(".intro-div").classList.remove("hidden");
+        document.querySelector(".welcome-msg").classList.add("hidden");
+    
+    });
+    }
+    
+
+}
